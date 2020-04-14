46857877e63d4c468b534dedc18a9434458c9035
malou-c <ksh1957nr@mail.ru>
2020-04-14 11:09:35 +0700

    Upgrade file.h and main.cpp
    now work!

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 
 int main() {
   File test;
+  char name[50] = "Voava";
+  test.add(name, 213811);
+  test.add(name, 21382);
 
-  test.add("PAPICH", 2138);
-  test.add("SOLO322", 214);
-
-  PersonStats for_test;
-
-  //for_test = test.load();
+  std::vector<PersonStats> vec = test.load();
+  for (int i = 0; i < (int)vec.size(); i++) {
+    std::cout << vec[i].name << " " << vec[i].time << std::endl;
+  }
 
   return 0;
 }
