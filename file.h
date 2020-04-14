1d81093c14afa78622dd89c17931b997abbbc882
malou-c <ksh1957nr@mail.ru>
2020-04-14 11:15:32 +0700

    Try commit file.h

--- file.h
+++ file.h
@@ -1,98 +1,49 @@
-#pragma once
+
+
+#include <string.h>
 
 #include <fstream>
-#include <string.h>
+#include <iostream>
 #include <vector>
 
-typedef struct List {
-  char name[20];
+class PersonStats {
+ public:
+  char name[50];
   float time;
-
-  struct List *next;
-} List;
-
-struct PersonStats {
-  std::vector<std::string> name;
-  std::vector<float> time;
 };
 
 class File {
-private:
-  std::string path = "lider board.dat";
+ private:
+  std::string path = "lider_board.dat";
 
-public:
-
-  void add(std::string name, float time) { //добавялет запись о пользователе в конец
+ public:
+  void add(char name[50],
+           float time) {  //добавялет запись о пользователе в конец
     PersonStats person;
-    int person_size;
 
-    std::fstream file;
-    std::ofstream w_file;
+    for (int i = 0; i < 50; i++) person.name[i] = name[i];
 
-    file.open(path, std::ios::binary | std::ios::in | std::ios::out);//открываем файл
+    person.time = time;
 
-    if (!file.is_open()) {//если не получилось найти файл то создаем его
-
-      std::ofstream create;
-      create.open(path, std::ios::binary);
-      create.close();
-
-      file.open(path, std::ios::binary | std::ios::in | std::ios::out);
-
-      if (!file.is_open()) {
-        std::cout << "Не получается создать файл" << std::endl;
-        return;
-      }
-    }
-
-    if (file.read((char *)&person_size, sizeof(int))){//если файл не пустой
-      std::cout << person_size;
-      file.read((char *)&person, person_size);
-      file.close();
-      w_file.open(path, std::ios::binary);
-      person.name.push_back(name);
-      person.time.push_back(time);
-
-      person_size = sizeof(person);
-
-      w_file.write((char *)&person_size, sizeof(int));
-      w_file.write((char *)&person, person_size);
-      w_file.close();
-    } else {//если файл пустой
-      file.close();
-      w_file.open(path, std::ios::binary);
-      person.name.push_back(name);
-      person.time.push_back(time);
-
-      person_size = sizeof(person);
-      //std::cout << person_size;
-
-      w_file.write((char *)&person_size, sizeof(int));
-      w_file.write((char *)&person, sizeof(person));
-      w_file.close();
-    }
-
+    std::ofstream file;
+    file.open(path, std::ios::binary | std::ios::out |
+                        std::ios::app);  //открываем файл
+    file.write((char *)&person, sizeof(PersonStats));
+    file.close();
   }
 
-/*  PersonStats load() {
-    PersonStats persons;
+  std::vector<PersonStats> load() {
+    std::vector<PersonStats> vec_per;
+    PersonStats perk;
+    std::ifstream file2;
 
-    std::ifstream file;
-
-    file.open(path, std::ios::binary);
-
-    if (!file.is_open()) {
-      std::cout << "Файл не найден" << std::endl;
-    } else {
-
-      file.read((char *)&persons, sizeof(PersonStats));
-      file.close();
-
-      std::cout << persons.name[0] << " " << persons.name[1];
-
-      return persons;
+    file2.open(path, std::ios::binary | std::ios::in);
+    while (file2.read((char *)&perk, sizeof(PersonStats))) {
+      // std::cout << perk.name << " " << perk.time << std::endl;
+      vec_per.push_back(perk);
     }
 
-
-  }*/
+    file2.close();
+    return vec_per;
+  }
 };
