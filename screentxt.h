abddfae25861ecf25897b25daf3e02322b84d103
Aleksandr <lilxtent@gmail.com>
2020-04-21 19:20:22 +0700

    Text formater with small problems(words carryover on new string)

--- screentxt.h
+++ screentxt.h
@@ -1,9 +1,42 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-
-std::wstring print_text()
+struct elem
 {
-    std::wstring str = L"В вестибюле пахло вареной капустой и старыми половиками. Против входа на стене висел цветной плакат, слишком большой для помещения. На плакате было изображено громадное, больше метра в ширину, лицо – лицо человека лет сорока пяти, с густыми черными усами, грубое, но по-мужски привлекательное. Уинстон направился к лестнице. К лифту не стоило и подходить. Он даже в лучшие времена редко работал, а теперь в дневное время электричество вообще отключали. Действовал режим экономии – готовились к Неделе ненависти. Уинстону предстояло одолеть семь маршей; ему шел сороковой год, над щиколоткой у него была варикозная язва; он поднимался медленно и несколько раз останавливался передохнуть. На каждой площадке со стены глядело все то же лицо. Портрет был выполнен так, что, куда бы ты ни стал, глаза тебя не отпускали. СТАРШИЙ БРАТ СМОТРИТ НА ТЕБЯ – гласила подпись.";
-    return str;
+    std::wstring str;
+};
+
+
+std::vector<std::vector<std::string>> get_text()
+{   int width = (1500 / 8) - 20;
+    std::string str = "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him. The hallway smelt of boiled cabbage and old rag mats. At one end of it a coloured poster, too large for indoor display, had been tacked to the wall. It depicted simply an enormous face, more than a metre wide: the face of a man of about forty-five, with a heavy black moustache and ruggedly handsome features. Winston made for the stairs. It was no use trying the lift. Even at the best of times it was seldom working, and at present the electric current was cut off during daylight hours. It was part of the economy drive in preparation for Hate Week. The flat was seven flights up, and Winston, who was thirty-nine and had a varicose ulcer above his right ankle, went slowly, resting several times on the way. On each landing, opposite the lift-shaft, the poster with the enormous face gazed from the wall. It was one of those pictures which are so contrived that the eyes follow you about when you move. BIG BROTHER IS WATCHING YOU, the caption beneath it ran.";
+
+    std::vector<std::vector<std::string>> text;
+
+    text.resize(1);
+
+    int j = 0;
+    int lineid = 0;
+    for (int i = 0; i < str.length() ; i++)
+    {
+        if (text[j].size() < width - 50)
+        {   
+            if (lineid == 0 && str[i] == ' ') continue;
+            text[j].resize(text[j].size() + 1);
+            text[j][lineid] = str[i];
+            lineid++;
+        } 
+        else
+        {   
+            lineid = 0;
+            j++;
+            i--;
+            text.resize(text.size() + 1);
+        }
+
+    }
+    j = 0;
+    lineid = 0;
+
+    return text;
 }
