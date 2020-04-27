9706c2176977d744681d9fdade6a07ada32a40d1
Netryle <d.a.n.2004@mail.ru>
2020-04-28 02:32:17 +0700

    Add start. stop and reset clock functions

--- clockface.cpp
+++ clockface.cpp
@@ -1,7 +1,7 @@
 #include "clockface.hpp"
 //конструктор
 ClockFace::ClockFace() {
-  font.loadFromFile("fonts/stdFont.ttf");
+  font.loadFromFile("fonts\\CyrilicOld.ttf");
   text.setFont(font);
   text.setCharacterSize(25);
   text.setFillColor(Color::Black);
@@ -11,23 +11,28 @@ ClockFace::ClockFace() {
 //функции
 
 void ClockFace::update_clock() {
-  if (time < 1000) {
-    time = (msstorage.getElapsedTime().asMilliseconds());
-  } else {
-    time = 0;
-    msstorage.restart();
-  }
+    
+    if (switcher == true) {
+        if (time < 1000) {
+            time = (msstorage.getElapsedTime().asMilliseconds());        
+        }
+        else {
+            time = 0;
+            msstorage.restart();
+        }
 
-  if (seconds < 60) {
-    seconds = secstorage.getElapsedTime().asSeconds();
-  } else {
-    seconds = 0;
-    secstorage.restart();
-  }
+        if (seconds < 60) {
+            seconds = (secstorage.getElapsedTime().asSeconds());
+        }
+        else {
+            seconds = 0;
+            secstorage.restart();
+        }
 
-  minsum = seconds / 60;
+        minsum = seconds / 60;
 
-  minutes += minsum;
+        minutes += minsum;
+    }
 }
 
 void ClockFace::DrawClock(RenderWindow &window) {
@@ -44,4 +49,34 @@ void ClockFace::DrawClock(RenderWindow &window) {
 void ClockFace::setPosition(int x, int y) {
   position.x = x;
   position.y = y;
+}
+
+void ClockFace::ClockReset()
+{
+    switcher = 0;
+    time = 0;
+    seconds = 0;
+    minutes = 0;
+}
+
+void ClockFace::ClockStart()
+{
+     msstorage.restart();
+     secstorage.restart();
+
+     switcher = 1;
+
+}
+
+void ClockFace::ClockStop()
+{
+    switcher = 0;
+
+}
+void ClockFace::ClockContinue()
+{
+    switcher = 1;
+    
+
+
 }
