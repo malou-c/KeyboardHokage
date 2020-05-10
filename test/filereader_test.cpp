bf256848979b18412937c8de480f2c4f619af61f
malou-c <ksh1957nr@mail.ru>
2020-05-10 15:51:14 +0700

    Add new test for filereader

---
+++ test/filereader_test.cpp
@@ -0,0 +1,38 @@
+#include "../src/filereader.hpp"
+#include "testparty/catch.hpp"
+#include <fstream>
+
+using namespace std;
+string path = "test/test_text.txt";
+
+void cleanfile(string path)
+{
+    //чистит файл перед тестом
+    fstream file;
+    file.open(path, ios::out | ios::trunc);
+    file.clear();
+    file.close();
+}
+template <class T>
+void writefile(T msg, string path)
+{
+    //чистит файл перед тестом
+    fstream file;
+    file.open(path, ios::out | ios::trunc);
+    file << msg;
+    file.close();
+}
+
+TEST_CASE("filereader")
+{
+    //записываем сообщение через функцию
+    wstring wmsg = L"test msg 1 2 3 ";
+    //записываем сообщение
+    string msg = "test msg 1 2 3";
+    cleanfile(path);
+    writefile(msg, path);
+    CHECK(readfile_to_wstr(path) == wmsg);
+    //проверяем неправильное сообщение
+    wstring wrong_msg = L"wrong test";
+    CHECK(readfile_to_wstr(path) != wrong_msg);
+}
