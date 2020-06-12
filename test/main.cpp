445131cf8092e762056601a0363335c9f381b989
Aleksandr <lilxtent@gmail.com>
2020-06-12 22:12:52 +0700

    New main for tests

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,8 @@
 #define CATCH_CONFIG_MAIN
 #include "testparty/catch.hpp"
 
-#include "filereader_test.hpp"
+#include "file_test.hpp"
+//#include "filereader_test.hpp"
 /*
 int ID = 0;                      // глобал  ID
 int height = 1000, width = 1900; // высота и ширина окна
