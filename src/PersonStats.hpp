b868430ff09ca77f0b1a6b5dcda3b7ba6c47bf22
Aleksandr <lilxtent@gmail.com>
2020-06-18 12:33:50 +0700

    Can enter text

--- src/PersonStats.hpp
+++ src/PersonStats.hpp
@@ -13,15 +13,27 @@ public:
     char text_name[50]; //название текста
     float time;
     float cps; // clicks per second
+    std::string temp_name = "";
+    sf::Font font;
+    sf::Text text;
 
-    bool pressed, released; //необходимы для корректной смены цвета при нажатии
-                            //на интерфейс ввода имени
+    bool pressed = false, //необходимы для корректной смены цвета при нажатии
+            released = true; //на интерфейс ввода имени
+
+    bool key_releasd;
 
     PersonStats()
     {
         background.setSize(sf::Vector2f(150.f, 30.f));
         background.setPosition(0, 0);
-        background.setFillColor(sf::Color::Green);
+        background.setFillColor(sf::Color::Blue);
+
+        this->font.loadFromFile("fonts/stdFont.ttf");
+
+        this->text.setPosition(5, 10);
+        this->text.setFont(this->font);
+        this->text.setCharacterSize(18);
+        this->text.setFillColor(sf::Color::Black);
     };
 
     void window_for_name_input(sf::RenderWindow& window, sf::Event event)
@@ -44,15 +56,36 @@ public:
         }
 
         if (released) {
-            if (background.getFillColor() == sf::Color::Green) {
-                background.setFillColor(sf::Color::Blue);
-            } else {
+            if (background.getFillColor() == sf::Color::Blue) {
                 background.setFillColor(sf::Color::Green);
+            } else {
+                background.setFillColor(sf::Color::Blue);
             }
         }
 
         this->released = false;
         ////////////////////////////////////////////////////////////////////////
+
+        if (background.getFillColor() == sf::Color::Blue
+            && event.type == sf::Event::TextEntered
+            && event.text.unicode < 128) {
+            switch (event.text.unicode) {
+            case 0x8:
+                if (temp_name.size() != 0)
+                    temp_name.erase(temp_name.size() - 1);
+                break;
+
+            default:
+                if (temp_name.size() <= 50) {
+                    temp_name += event.text.unicode;
+                }
+                break;
+            }
+
+            this->text.setString(this->temp_name);
+        }
+
         window.draw(background);
+        window.draw(this->text);
     };
 };
