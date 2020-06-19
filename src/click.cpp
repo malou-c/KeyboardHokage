5d5b8d99239cd35860d288fa1b782cb9dd71b549
malou-c <ksh1957nr@mail.ru>
2020-06-19 23:32:36 +0700

    Add new function isRelised

--- src/click.cpp
+++ src/click.cpp
@@ -1,13 +1,23 @@
 #include "click.hpp"
 
-bool Click::isClicked(
-        sf::Sprite sprite, sf::RenderWindow& window, sf::Event event)
+bool Click::isClicked(Sprite sprite, RenderWindow& window, Event event)
 {
     //Если мышк внутри кнопки и была отпущена левая кнопка
-    if (sf::IntRect(sprite.getLocalBounds())
-                .contains(sf::Mouse::getPosition(window))
+    if (sf::IntRect(sprite.getGlobalBounds())
+                .contains(Mouse::getPosition(window))
         && event.type == event.MouseButtonPressed
-        && event.mouseButton.button == sf::Mouse::Left) {
+        && event.mouseButton.button == Mouse::Left) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Click::isReleased(Event event)
+{
+    //Если мышк внутри кнопки и была отпущена левая кнопка
+    if (event.type == event.MouseButtonReleased
+        && event.mouseButton.button == Mouse::Left) {
         return true;
     } else {
         return false;
--- src/click.hpp
+++ src/click.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
 
 class Click {
 public:
-    bool
-    isClicked(sf::Sprite sprite, sf::RenderWindow& window, sf::Event event);
+    bool relise_L = true;
+    bool relise_R = true;
+    //функции
+    bool isClicked(Sprite sprite, RenderWindow& window, Event event);
+    bool isReleased(Event event);
 };
