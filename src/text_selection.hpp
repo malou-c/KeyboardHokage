4fc666ac435bde6dcb77f5e9c6ac8074e4c45476
malou-c <ksh1957nr@mail.ru>
2020-06-19 23:34:28 +0700

    Add all function for normal work

--- src/text_selection.hpp
+++ src/text_selection.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Folder.hpp"
 #include "click.hpp"
+#include "screentxt.hpp"
 #include "section.hpp"
 #include <SFML/Graphics.hpp>
 #include <cstring>
@@ -21,15 +22,31 @@ public:
     Sprite background;
     //вектор из секций
     std::vector<Section> sections;
+    int curr_sect = -1;
+    Color color_curr = Color(20, 20, 150);
+    Color color_contain = Color(20, 20, 255);
     //отступы для секций
     Vector2i indent_sect;
-    int count_section; // кол-во секций
+    size_t count_section = 10; // кол-во секций
+    size_t start_sect = 0;
+    size_t count_draw_sect;
     //вектор из названий текстов
     std::vector<std::string> name_folders;
+    //кнопки << >>
+    Color disable_color = Color(255, 255, 255, 70);
+    bool but_relise = true;
+    Vector2f but_pos;
+    Texture texture_but_left;
+    Sprite but_left;
+    Texture texture_but_right;
+    Sprite but_right;
 
     //конструктор
     TextSelection(int x, int y);
     //функции
+    void update_sections(TextWindow& txtwin, Event event, RenderWindow& window);
+    void but_update(Event event, RenderWindow& window);
+    void recount_sect();
 
     // draw
     void draw(RenderWindow& window);
