a688de3369a818251ffafb4ee1c21036523d824e
malou-c <ksh1957nr@mail.ru>
2020-06-20 13:43:50 +0700

    try fix HelpButton text

--- test/HelpButton_test.cpp
+++ test/HelpButton_test.cpp
@@ -4,25 +4,16 @@
 TEST_CASE("HelpButton")
 {
     HelpButton help;
-    SECTION("MoveRight")
-    {
-        help.page_switch = 0;
-        int page_switch_test = 0;
-        for (int i = 0; i < 5; i++) {
-            page_switch_test++;
-            help.MoveRight();
-            CHECK(page_switch_test == help.page_switch);
-        }
-    }
-
-    SECTION("MoveLeft")
-    {
-        help.page_switch = 5;
-        int page_switch_test = 5;
-        for (int i = 5; i > 0; i--) {
-            page_switch_test--;
-            help.MoveLeft();
-            CHECK(page_switch_test == help.page_switch);
-        }
-    }
+    int page_switch_test = 0;
+    help.page_switch = 0;
+    // направо
+    page_switch_test++;
+    help.MoveRight();
+    CHECK(page_switch_test == help.page_switch);
+    //налево
+    help.page_switch = 5;
+    page_switch_test = 5;
+    page_switch_test--;
+    help.MoveLeft();
+    CHECK(page_switch_test == help.page_switch);
 }
