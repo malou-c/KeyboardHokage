fca14da0362da3545d694847d104edc1f5d3592e
Aleksandr <lilxtent@gmail.com>
2020-06-27 13:51:58 +0700

    Folder::contains only read .txt  files

--- src/Folder.cpp
+++ src/Folder.cpp
@@ -8,10 +8,18 @@ Folder::Folder(std::string path)
 //возвращает имена фалов в дирректории без названия самой дирректории
 std::vector<std::string> Folder::contains()
 {
+    std::string file_name;
     std::vector<std::string> list;
+
     for (auto& name : std::filesystem::directory_iterator(this->path)) {
-        //здесь удаляется имя дериктории
-        list.push_back(name.path().u8string().erase(0, path.size() + 1));
+        file_name = name.path().u8string().erase(0, path.size() + 1);
+
+        if (file_name[file_name.size() - 1] == 't'
+            && file_name[file_name.size() - 2] == 'x'
+            && file_name[file_name.size() - 3] == 't'
+            && file_name[file_name.size() - 4] == '.') {
+            list.push_back(file_name); //здесь удаляется имя дериктории
+        }
     }
     return list;
 };
